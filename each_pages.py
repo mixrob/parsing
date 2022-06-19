01f005d4533c2797db1b577306d1dc20c359a0c6
mixrob <perelyginpv@gmail.com>
2022-06-19 19:07:04 +0300

    parse all cards in pages with delay + save in file

--- each_pages.py
+++ each_pages.py
@@ -2,31 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"}
 
-for count in range(1, 8):
+def get_url():
 
-    sleep(1)
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
+    for count in range(1, 8):
 
-    response = requests.get(url, headers=headers)
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={count}"
 
-    soup = BeautifulSoup(response.text, "lxml")
+        response = requests.get(url, headers=headers)
 
-    data = soup.find_all("div", class_="col-lg-4 col-md-6 mb-4")
+        soup = BeautifulSoup(response.text, "lxml")
 
-    for i in data:
+        data = soup.find_all("div", class_="col-lg-4 col-md-6 mb-4")
 
-        card_url = "https://scrapingclub.com" + i.find("a").get("href")
-        list_card_url.append(card_url)
+        for i in data:
 
+            card_url = "https://scrapingclub.com" + i.find("a").get("href")
+            yield card_url
 
-    for card_url in list_card_url:
+def array_data():
+    for card_url in get_url():
 
         response = requests.get(card_url, headers=headers)
-
+        sleep(1)
         soup = BeautifulSoup(response.text, "lxml")
 
         data = soup.find("div", class_="card mt-4 my-4")
@@ -34,6 +34,7 @@ for count in range(1, 8):
         price = data.find("h4").text
         text = data.find("p", class_="card-text").text
         url_img = "https://scrapingclub.com" + data.find("img", class_="card-img-top img-fluid").get("src")
+        yield name, price, text, url_img
 
 
 
